ce15d5380f2fd47fc7bd244fc0761de26e25ddcc
Damaris <damarisrmarscal@gmail.com>
2020-09-10 17:57:57 -0500

    Tic Tac Toe

--- archivos gato/funciones.js
+++ archivos gato/funciones.js
@@ -1,22 +1,44 @@
 let turnoJugador1 = true
 let turnoJugador2 = true
 
-function marcarCasilla(numero) {
-  let casilla = document.getElementById("c" + numero);
-  casilla.childNodes[0].innerText = "X"
 
-  if (turnoJugador1) {
-    casilla.childNodes[0].innerText = "X";
-    casilla.classList.add("casilla-rosa");
-    casilla.classList.remove("casilla-amarilla");
-    turnoJugador1 = false
+function marcarCasilla(numero) {
+  let casilla = document.getElementById("casilla" + numero);
+
+  let ocupada = estaOcupada (casilla)
+  if (!ocupada){
+    if (turnoJugador1) {
+      casilla.childNodes[0].innerText = "X";
+      casilla.classList.add("casilla-rosa");
+      casilla.classList.remove("casilla-amarilla");
+      turnoJugador1 = false
+    }
+    else {
+      casilla.childNodes[0].innerText = "O";
+      casilla.classList.add("casilla-amarilla");
+      casilla.classList.remove("casilla-rosa");
+      turnoJugador1 = true
+    }
   }
-  else {
-    casilla.childNodes[0].innerText = "0";
-    casilla.classList.add("casilla-amarilla");
-    casilla.classList.remove("casilla-rosa");
-    turnoJugador1 = true
+  }
+
+function estaOcupada(casilla){
+  if (casilla.childNodes[0].innerText) {
+    console.log("si esta ocupado");
+    return true;
+  } else {
+    console.log("no esta ocupado");
+    return false;
   }
 }
 
-console.log("Jd");
+function reiniciarJuego(){
+  (--! reiniciarJuego x 9--)
+}
+
+function limpiarCasilla(numero){
+  let casilla = document.getElementById("casilla" + numero)
+  casilla.childNodes[0].innerText = "";
+  casilla.classList.remove("casilla-amarilla");
+  casilla.classList.remove("casilla-rosa");
+}
